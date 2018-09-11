760dd3f3c6796c2a9c8156456c936b8f9028c146
Stefan Bauer <stefan.bauer@n8d.at>
2018-09-11 11:28:11 +0200

    Update gulpfile for handlebars

--- generators/handlebars/templates/gulpfile.js
+++ generators/handlebars/templates/gulpfile.js
@@ -1,3 +1,4 @@
+// definition of Handlebars loader
 const loaderConfig = {
   test: /\.hbs/,
   loader: 'handlebars-template-loader'
@@ -12,17 +13,44 @@ build.configureWebpack.mergeConfig({
     return generatedConfiguration;
 
   }
+
 });
+
+// marker to check if custom watch is already registered
+// used to prevent watch bubbling
+let customWatchRegistered = false;
 
 // Register watches sub task to move hbs files over to libs directory
 let hbsWatch = build.subTask('hbsWatch', (gulp, buildOptions, done) => {
 
-  gulp.watch('./**/*.hbs', () => {
+  // register watch only on first launch
+  if (!customWatchRegistered) {
 
+    // on change of +.hbs files
+    gulp.watch('./src/**/*.hbs', event => {
+
+
+      // copy hbs from src to lib
+      gulp.src('./src/**/*.hbs')
+        .pipe(gulp.dest('./lib/'));
+
+      // copy empty index.ts to itself to launch build procees
+      gulp.src('./src/index.ts')
+        .pipe(gulp.dest('./src/'));
+
+    });
+
+    // after watch is registered don't register again
+    // to avoid event bubbling
+    customWatchRegistered = true;
+
+  } else {
+
+    // make sure preBuild file will be copied once again
     gulp.src('./src/**/*.hbs')
       .pipe(gulp.dest('./lib/'));
 
-  });
+  }
 
   done();
 
