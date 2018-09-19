3c8301bc30db732d28d97a9c250147c51546fcb5
Stefan Bauer <stefan.bauer@n8d.at>
2018-09-19 08:40:04 +0200

    Added PnP Reusable controls for React Projects

--- generators/addons/promptConfig.js
+++ generators/addons/promptConfig.js
@@ -41,6 +41,11 @@ const pnpJsOptions = [{
     }
 ]
 
+const reactLibs = [{
+    name: 'PnP Reusable Controls',
+    value: '@pnp/spfx-controls-react'
+}];
+
 const defaultLibs = [{
         name: 'jQuery',
         value: 'jquery'
@@ -58,7 +63,16 @@ const configOptions = [
         type: 'checkbox',
         message: 'Which libraries to include',
         name: 'jsLibrary',
-        choices: defaultLibs
+        choices: answers => {
+
+            switch (answers.framework) {
+                case "react":
+                    return defaultLibs.concat(reactLibs);
+                default:
+                    return defaultLibs;
+            }
+
+        }
     },
     // jQuery version selection
     {
