036689f615ccd889880c84e59fb997d88dca9c85
Stefan Bauer <stefan.bauer@n8d.at>
2018-10-07 21:47:21 +0200

    Update watch tasks in handlebars and vuejs

--- generators/handlebars/templates/gulpfile.js
+++ generators/handlebars/templates/gulpfile.js
@@ -16,42 +16,13 @@ build.configureWebpack.mergeConfig({
 
 });
 
-// marker to check if custom watch is already registered
-// used to prevent watch bubbling
-let customWatchRegistered = false;
+// register custom watch for hbbs.JS files
+// copy of '.hba' files will be handled by 'copy-static-assets.json'
+gulp.watch('./src/**/*.hbs', event => {
 
-// Register watches sub task to move hbs files over to libs directory
-let hbsWatch = build.subTask('hbsWatch', (gulp, buildOptions, done) => {
-
-  // register watch only on first run
-  if (!customWatchRegistered) {
-
-    // on change of *.hbs files
-    gulp.watch('./src/**/*.hbs', event => {
-
-      // copy hbs from src to lib
-      gulp.src('./src/**/*.hbs')
-        .pipe(gulp.dest('./lib/'));
-
-      // copy empty index.ts onto itself to launch build procees
-      gulp.src('./src/index.ts')
-        .pipe(gulp.dest('./src/'));
-
-    });
-
-    // after watch is registered don't register again
-    customWatchRegistered = true;
-
-  } else {
-
-    // make sure preBuild file will be copied once again
-    gulp.src('./src/**/*.hbs')
-      .pipe(gulp.dest('./lib/'));
-
-  }
-
-  // tell build.rig the work is done.
-  done();
+    // copy empty index.ts onto itself to launch build procees
+    gulp.src('./src/index.ts')
+      .pipe(gulp.dest('./src/'));
 
 });
 
--- generators/vuejs/templates/gulpfile.js
+++ generators/vuejs/templates/gulpfile.js
@@ -60,27 +60,12 @@ build.configureWebpack.mergeConfig({
 
 });
 
-// marker to check if custom watch is already registered
-// used to prevent watch bubbling
-let customWatchRegistered = false;
-
-let watchVueFiles = build.subTask('watch-vue-files', function (gulp, buildOptions, done) {
-    // register watch only on first run
-    if (!customWatchRegistered) {
-
-        // on change of *.vue files
-        gulp.watch('./src/**/*.vue', event => {
-            // copy empty index.ts onto itself to launch build procees
-            gulp.src('./src/index.ts')
-                .pipe(gulp.dest('./src/'));
-        });
-
-        // after watch is registered don't register again
-        customWatchRegistered = true;
-
-    }
-
-    done();
+// register custom watch for Vue.JS files
+// copy of '.vue' files will be handled by 'copy-static-assets.json'
+gulp.watch('./src/**/*.vue', event => {
+    // copy empty index.ts onto itself to launch build procees
+    gulp.src('./src/index.ts')
+        .pipe(gulp.dest('./src/'));
 });
 
 build.rig.addPreBuildTask(watchVueFiles);
