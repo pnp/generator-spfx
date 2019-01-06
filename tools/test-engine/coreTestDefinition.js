77edd1b34b3b77f3ce2f1e44e3394785349edb2a
Stefan Bauer <stefan.bauer@n8d.at>
2019-01-06 05:27:05 +0100

    Webpack Analyser Tests added

--- tools/test-engine/coreTestDefinition.js
+++ tools/test-engine/coreTestDefinition.js
@@ -176,7 +176,8 @@ class TestGenerator {
 
         // generate all tests
         this._generateTests();
-        this._generateVettingTests();
+        this._generateStyleLintTests();
+        this._generateWebPackTests();
 
     }
 
@@ -576,7 +577,449 @@ class TestGenerator {
 
     }
 
-    _generateVettingTests() {
+    _generateWebPackTests() {
+
+        const noJQuery = Object.assign({}, this.baseTest);
+        const fileContent = new FileContent();
+        const testType = new TestType();
+
+        /**
+         * No jQuery Test Definition
+         */
+        noJQuery.name = " No jQuery";
+        noJQuery.specifics = {
+            vetting: ['webpack-analyzer']
+        };
+        noJQuery.test = this.baseTest.test.concat([{
+                name: 'no jquery 2.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^2\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'no @types/jquery 2.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^2\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'no jquery 3.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^3\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'no @types/jquery 3.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^3\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'no pnpjs',
+                file: fileContent.package,
+                expr: /@pnp\/pnpjs/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @pnp/spfx-property-controls',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-property-controls/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @pnp/spfx-controls-react',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-controls-react/,
+                type: testType.noFileContent
+            }
+            // ????
+            // ,
+            // {
+            //     name: 'Webpack analyser',
+            //     file: fileContent.package,
+            //     expr: /"webpack-bundle-analyzer"/,
+            //     type: testType.fileContent
+            // }
+        ])
+
+        this._tests.push(noJQuery);
+
+        /**
+         * jQuery 2 test
+         */
+
+        const jquery2 = Object.assign({}, this.baseTest);
+        jquery2.name = " jQuery 2.x.x";
+        jquery2.specifics = {
+            jsLibrary: ['jquery'],
+            vetting: ['webpack-analyzer'],
+            jQueryVersion: 2,
+            force: true
+        };
+        jquery2.test = this.baseTest.test.concat([{
+                name: 'no jquery 2.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^2\./,
+                type: testType.fileContent
+            },
+            {
+                name: 'no @types/jquery 2.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^2\./,
+                type: testType.fileContent
+            },
+            {
+                name: 'no jquery 3.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^3\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'no @types/jquery 3.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^3\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'no pnpjs',
+                file: fileContent.package,
+                expr: /@pnp\/pnpjs/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @pnp/spfx-property-controls',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-property-controls/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @pnp/spfx-controls-react',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-controls-react/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'Webpack analyser',
+                file: fileContent.package,
+                expr: /"webpack-bundle-analyzer"/,
+                type: testType.fileContent
+            }
+        ])
+
+        this._tests.push(jquery2);
+
+        /**
+         * jQuery 3 Test Definition
+         */
+        const jquery3 = Object.assign({}, this.baseTest);
+        jquery3.name = " jQuery 3.x.x";
+        jquery3.specifics = {
+            jsLibrary: ['jquery'],
+            vetting: ['webpack-analyzer'],
+            jQueryVersion: 3,
+            force: true
+        };
+        jquery3.test = this.baseTest.test.concat([{
+                name: 'no jquery 2.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^2\./,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @types/jquery 2.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^2\./,
+                type: testType.noFileContent
+            },
+            {
+                name: 'jquery 3.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^3\./,
+                type: testType.fileContent
+            },
+            {
+                name: '@types/jquery 3.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^3\./,
+                type: testType.fileContent
+            },
+            {
+                name: 'no pnpjs',
+                file: fileContent.package,
+                expr: /@pnp\/pnpjs/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @pnp/spfx-property-controls',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-property-controls/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @pnp/spfx-controls-react',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-controls-react/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'Webpack analyser',
+                file: fileContent.package,
+                expr: /"webpack-bundle-analyzer"/,
+                type: testType.fileContent
+            }
+        ])
+
+        this._tests.push(jquery3);
+
+        /**
+         * jQuer 2, pnpjs test
+         */
+        const jqueryPnPJS2 = Object.assign({}, this.baseTest);
+        jqueryPnPJS2.name = " jQuery 2.x.x, pnpjs, ";
+        jqueryPnPJS2.specifics = {
+            jsLibrary: ['jquery', '@pnp/pnpjs'],
+            vetting: ['webpack-analyzer'],
+            jQueryVersion: 2,
+            force: true
+        };
+        jqueryPnPJS2.test = this.baseTest.test.concat([{
+                name: 'jquery 2.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^2\./,
+                type: testType.fileContent
+            },
+            {
+                name: '@types/jquery 2.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^2\./,
+                type: testType.fileContent
+            },
+            {
+                name: 'no jquery 3.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^3\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'no @types/jquery 3.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^3\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'pnpjs',
+                file: fileContent.package,
+                expr: /@pnp\/pnpjs/,
+                type: testType.fileContent
+            },
+            {
+                name: 'no @pnp/spfx-property-controls',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-property-controls/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @pnp/spfx-controls-react',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-controls-react/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'Webpack analyser',
+                file: fileContent.package,
+                expr: /"webpack-bundle-analyzer"/,
+                type: testType.fileContent
+            }
+        ])
+
+        this._tests.push(jqueryPnPJS2);
+
+        /**
+         * jquery 3, pnpjs test
+         */
+        const jqueryPnPJS3 = Object.assign({}, this.baseTest);
+        jqueryPnPJS3.name = " jQuery 3.x.x, pnpjs";
+        jqueryPnPJS3.specifics = {
+            jsLibrary: ['jquery', '@pnp/pnpjs'],
+            vetting: ['webpack-analyzer'],
+            jQueryVersion: 3,
+            force: true
+        };
+        jqueryPnPJS3.test = this.baseTest.test.concat([{
+                name: 'no jquery 2.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^2\./,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @types/jquery 2.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^2\./,
+                type: testType.noFileContent
+            },
+            {
+                name: 'jquery 3.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^3\./,
+                type: testType.fileContent
+            },
+            {
+                name: '@types/jquery 3.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^3\./,
+                type: testType.fileContent
+            },
+            {
+                name: 'pnpjs',
+                file: fileContent.package,
+                expr: /@pnp\/pnpjs/,
+                type: testType.fileContent
+            },
+            {
+                name: 'no @pnp/spfx-property-controls',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-property-controls/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @pnp/spfx-controls-react',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-controls-react/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'Webpack analyser',
+                file: fileContent.package,
+                expr: /"webpack-bundle-analyzer"/,
+                type: testType.fileContent
+            }
+        ])
+
+        this._tests.push(jqueryPnPJS3);
+
+        /**
+         * jquery 2 pnpjs, property control
+         */
+        const jqueryPropPnPJS2 = Object.assign({}, this.baseTest);
+        jqueryPropPnPJS2.name = " jQuery 2.x.x, pnp/pnpjs, @pnp/spfx-property-controls";
+        jqueryPropPnPJS2.specifics = {
+            jsLibrary: ['jquery', '@pnp/pnpjs', '@pnp/spfx-property-controls'],
+            vetting: ['webpack-analyzer'],
+            jQueryVersion: 2,
+            force: true
+        };
+        jqueryPropPnPJS2.test = this.baseTest.test.concat([{
+                name: 'no jquery 2.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^2\./,
+                type: testType.fileContent
+            },
+            {
+                name: '@types/jquery 2.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^2\./,
+                type: testType.fileContent
+            },
+            {
+                name: 'no jquery 3.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^3\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'no @types/jquery 3.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^3\./,
+                type: testType.nofileContent
+            },
+            {
+                name: 'pnpjs',
+                file: fileContent.package,
+                expr: /@pnp\/pnpjs/,
+                type: testType.fileContent
+            },
+            {
+                name: '@pnp/spfx-property-controls',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-property-controls/,
+                type: testType.fileContent
+            },
+            {
+                name: 'no @pnp/spfx-controls-react',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-controls-react/,
+                type: testType.noFileContent
+            }
+        ])
+
+        this._tests.push(jqueryPropPnPJS2);
+
+        /**
+         * jQUery 3, pnpjs, property control
+         */
+        const jqueryPropPnPJS3 = Object.assign({}, this.baseTest);
+        jqueryPropPnPJS3.name = " jQuery 3.x.x, pnpjs, @pnp/spfx-property-controls";
+        jqueryPropPnPJS3.specifics = {
+            jsLibrary: ['jquery', '@pnp/pnpjs', '@pnp/spfx-property-controls'],
+            vetting: ['webpack-analyzer'],
+            jQueryVersion: 3,
+            force: true
+        };
+        jqueryPropPnPJS3.test = this.baseTest.test.concat([{
+                name: 'no jquery 2.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^2\./,
+                type: testType.noFileContent
+            },
+            {
+                name: 'no @types/jquery 2.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^2\./,
+                type: testType.noFileContent
+            },
+            {
+                name: 'jquery 3.x',
+                file: fileContent.package,
+                expr: /\"jquery\": \"\^3\./,
+                type: testType.fileContent
+            },
+            {
+                name: '@types/jquery 3.x',
+                file: fileContent.package,
+                expr: /\"@types\/jquery\": \"\^3\./,
+                type: testType.fileContent
+            },
+            {
+                name: 'pnpjs',
+                file: fileContent.package,
+                expr: /@pnp\/pnpjs/,
+                type: testType.fileContent
+            },
+            {
+                name: '@pnp/spfx-property-controls',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-property-controls/,
+                type: testType.fileContent
+            },
+            {
+                name: 'no @pnp/spfx-controls-react',
+                file: fileContent.package,
+                expr: /@pnp\/spfx-controls-react/,
+                type: testType.noFileContent
+            },
+            {
+                name: 'Webpack analyser',
+                file: fileContent.package,
+                expr: /"webpack-bundle-analyzer"/,
+                type: testType.fileContent
+            }
+        ])
+
+        this._tests.push(jqueryPropPnPJS3);
+
+    }
+
+    _generateStyleLintTests() {
 
         const noJQuery = Object.assign({}, this.baseTest);
         const fileContent = new FileContent();
