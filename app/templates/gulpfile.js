4378558d21dba2e94c78d47e148a6bbd1b04f294
Stefan Bauer <stefan.bauer@n8d.at>
2019-01-14 09:43:15 +0100

    Gulptask will included now via for loop instead of if statements
    Update for pnp-reuseable-controls for reactjs.plus generator

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,34 +1,48 @@
+<%= SpfxOptions %>
 <%
 
-var webpackAnalyzer = false;
-var stylelint = false;
+var allGulpTasks = [];
 
 if (undefined !== SpfxOptions) {
 
     // Check for webpack bundle analyzer
     if(SpfxOptions['pnp-vetting'].indexOf('webpack-analyzer') !== -1){
-        webpackAnalyzer = true;
+
+        let gulptaskPath = options.path+'webpack-bunde-analyzer.js';
+
+        allGulpTasks.push({
+            name: 'webpack-bundle-analyzer',
+            path: gulptaskPath,
+            data: {}
+        });
+
     }
 
     if(SpfxOptions['pnp-vetting'].indexOf('stylelint') !== -1){
-        stylelint = true;
+
+        let gulptaskPath = options.path+'stylelint.js';
+
+        allGulpTasks.push({
+            name: 'webpack-bundle-analyzer',
+            path: gulptaskPath,
+            data: {}
+        });
+
     }
 
 }
 
-%>'use strict';
+%>
+'use strict';
 const path = require('path');
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
-<% if (webpackAnalyzer) { %>
-<%- include(`${options.path}/webpack-bunde-analyzer.js`, {}); %>
-<% } %>
-<% if (stylelint) { %>
-<%- include(`${options.path}/stylelint.js`, {}); %>
-<% } %>
+<% allGulpTasks.forEach(element => {%>
+<%- include(element.path, element.data); _%>
+<% }); %>
 
 /**
  * Custom Framework Specific gulp tasks
