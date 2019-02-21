add1b9ebddc9ae85cc344070e705407e13174b4e
Vincent Biret <vincentbiret@hotmail.com>
2019-02-21 14:14:55 -0500

    fixed a bug where renaming and changing question type of CI had broken CI files deployment

--- app/index.js
+++ app/index.js
@@ -113,7 +113,7 @@ module.exports = class extends Generator {
                     this.config.set('framework', this.options.SpfxOptions['framework']);
                     this.config.set('pnpFramework', this.options.pnpFramework);
                     this.config.set('pnp-libraries', this.options.libraries);
-                    this.config.set('pnp-ci', this.options.continuousIntegration);
+                    this.config.set('pnp-ci', this.options.ci);
                     this.config.set('pnp-vetting', this.options.vetting);
                     this.config.set('spfxenv', this.options.SpfxOptions['environment']);
                     this.config.save();
