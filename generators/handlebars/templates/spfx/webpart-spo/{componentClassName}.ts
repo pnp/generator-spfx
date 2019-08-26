2f48d9bcaf8e111352d1d3428f94fcecefde7503
Stefan Bauer <stefan.bauer@n8d.at>
2019-08-26 09:51:46 +0200

    Upgrade to version SPFx 1.9.1
    Added SPO Library components

--- generators/handlebars/templates/spfx/webpart-spo/{componentClassName}.ts
+++ generators/handlebars/templates/spfx/webpart-spo/{componentClassName}.ts
@@ -1,9 +1,9 @@
 import { Version } from '@microsoft/sp-core-library';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import {
-  IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
+    BaseClientSideWebPart,
+    IPropertyPaneConfiguration,
+    PropertyPaneTextField
+  } from '@microsoft/sp-webpart-base';
 import { escape } from '@microsoft/sp-lodash-subset';
 
 import styles from './<%= componentClassName %>.module.scss';
